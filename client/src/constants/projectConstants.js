b87ef4b9c3a0f9fc4946c7c7356a24a59e359237
Aloha <utah.clayton@gmail.com>
2021-09-18 00:24:15 -0700

    added hard coded organizations to constants

--- client/src/constants/projectConstants.js
+++ client/src/constants/projectConstants.js
@@ -1,25 +1,114 @@
 
-export const projects = [
-    {
-      title: '',
-      description: "",
-        image: '',
-        tags: [],
-      source: '',
-      code: '',
-      id: 0,
-    },
-    {
-        title: 'Tech Blog',
-        description: "Save information to a database. While sign in a user can make new posts which are in turn saved for future edit and deletion if one chooses to. ",
-        image: '/images/tech.png',
-        tags: ['Javacript ', 'Node.js', 'MySQL', 'Sequelize'],
-        source:'https://only-tech-news.herokuapp.com',
-        code:'https://github.com/Scuobahia/only-tech-news' ,
-        id: 7,
-      },
-    
-  ];
+export const organizations = [
+  // humanitarian
+  {
+    title: 'CARE International',
+    description: "CARE is a major international humanitarian agency delivering emergency relief and long-term international development projects. Founded in 1945, CARE is nonsectarian, impartial, and non-governmental. It is one of the largest and oldest humanitarian aid organizations focused on fighting global poverty.",
+    image: '',
+    tags: [],
+    site: 'https://www.care.org/',
+    id: 0,
+  },
+  {
+    title: 'International Rescue Committee',
+    description: "The International Rescue Committee responds to the world’s worst humanitarian crises and helps people whose lives and livelihoods are shattered by conflict and disaster to survive, recover and gain control of their future.",
+    image: '',
+    tags: [],
+    site: 'https://www.rescue.org/',
+    id: 1,
+  },
+  {
+    title: 'American Red Cross',
+    description: "The American Red Cross is a non-profit humanitarian organization that provides emergency assistance, disaster relief, and disaster preparedness education in the United States.",
+    image: '',
+    tags: [],
+    site: 'https://www.redcross.org/',
+    id: 2,
+  },
+  {
+    title: 'The World Food Programme',
+    description: "The World Food Programme is the food-assistance branch of the United Nations. It is the world's largest humanitarian organization, the largest one focused on hunger and food security, and the largest provider of school meals.",
+    image: '',
+    tags: [],
+    site: 'https://www.wfpusa.org/',
+    id: 3,
+  },
+  {
+    title: 'Doctors Without Borders',
+    description: "Médecins Sans Frontières, sometimes rendered in English as Doctors Without Borders, is an international humanitarian medical non-governmental organisation of French origin best known for its projects in conflict zones and in countries affected by endemic diseases.",
+    image: '',
+    tags: [],
+    site: 'https://www.doctorswithoutborders.org/',
+    id: 4,
+  },
+  {
+    title: 'Oxfam',
+    description: "Oxfam is a British founded confederation of 20 independent charitable organizations focusing on the alleviation of global poverty, founded in 1942 and led by Oxfam International. It is a major nonprofit group with an extensive collection of operations.",
+    image: '',
+    tags: [],
+    site: 'https://www.oxfamamerica.org/',
+    id: 5,
+  },
+// Nature/Wildlife
+  {
+    title: 'The Nature Conservancy',
+    description: "Our mission is to conserve the lands and waters on which all life depends. To achieve this, we must boldly address the biodiversity and climate crises over the next decade.",
+    image: '',
+    tags: [],
+    site: 'https://www.nature.org/en-us/',
+    id: 6,
+  },
+  {
+    title: 'https://www.nrdc.org/',
+    description: "NRDC works to safeguard the earth—its people, its plants and animals, and the natural systems on which all life depends.",
+    image: '',
+    tags: [],
+    site: 'https://www.nrdc.org/',
+    id: 7,
+  },
+  {
+    title: 'World Wildlife Fund',
+    description: "Our mission is to conserve nature and reduce the most pressing threats to the diversity of life on Earth.",
+    image: '',
+    tags: [],
+    site: 'https://www.worldwildlife.org/',
+    id: 8,
+  },
+  {
+    title: 'The American Society for the Prevention of Cruelty to Animals',
+    description: "The American Society for the Prevention of Cruelty to Animals is a non-profit organization dedicated to preventing cruelty to animals.",
+    image: '',
+    tags: [],
+    site: 'https://www.aspca.org/',
+    id: 9,
+  },
+  {
+    title: 'Greenpeace',
+    description: "Greenpeace is a non-governmental environmental organisation with offices in over 55 countries and an international coordinating body in Amsterdam, the Netherlands. Greenpeace was founded in 1971 by Irving Stowe and Dorothy Stowe, Canadian and US expat environmental activists.",
+    image: '',
+    tags: [],
+    site: 'https://www.greenpeace.org/usa/',
+    id: 10,
+  },
+  {
+    title: 'The Sierra Club',
+    description: "The Sierra Club is the most enduring and influential grassroots environmental organization in the United States. We amplify the power of our 3.8 million members and supporters to defend everyone’s right to a healthy world.",
+    image: '',
+    tags: [],
+    site: 'https://www.sierraclub.org/',
+    id: 11,
+  },
   
-  export const TimeLineData = [
-  ];
+  {
+    title: 'Tech Blog',
+    description: "Save information to a database. While sign in a user can make new posts which are in turn saved for future edit and deletion if one chooses to. ",
+    image: '/images/tech.png',
+    tags: ['Javacript ', 'Node.js', 'MySQL', 'Sequelize'],
+    source: 'https://only-tech-news.herokuapp.com',
+    id: 7,
+  },
+
+];
+
+export const TimeLineData = [
+];
