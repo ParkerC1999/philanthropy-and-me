4fefe3e446cd5709e7cf5f01b06440374d62d931
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-09-17 20:05:12 -0600

    react structure setup

---
+++ client/src/constants/projectConstants.js
@@ -0,0 +1,25 @@
+
+export const projects = [
+    {
+      title: '',
+      description: "",
+        image: '',
+        tags: [],
+      source: '',
+      code: '',
+      id: 0,
+    },
+    {
+        title: 'Tech Blog',
+        description: "Save information to a database. While sign in a user can make new posts which are in turn saved for future edit and deletion if one chooses to. ",
+        image: '/images/tech.png',
+        tags: ['Javacript ', 'Node.js', 'MySQL', 'Sequelize'],
+        source:'https://only-tech-news.herokuapp.com',
+        code:'https://github.com/Scuobahia/only-tech-news' ,
+        id: 7,
+      },
+    
+  ];
+  
+  export const TimeLineData = [
+  ];
