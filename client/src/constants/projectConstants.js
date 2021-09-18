2a1df96d545f0dcb37cc2a9583e65969fa8fb8ba
Aloha <utah.clayton@gmail.com>
2021-09-18 07:41:00 -0700

    added images to components and images folder

--- client/src/constants/projectConstants.js
+++ client/src/constants/projectConstants.js
@@ -4,7 +4,7 @@ export const organizations = [
   {
     title: 'CARE International',
     description: "CARE is a major international humanitarian agency delivering emergency relief and long-term international development projects. Founded in 1945, CARE is nonsectarian, impartial, and non-governmental. It is one of the largest and oldest humanitarian aid organizations focused on fighting global poverty.",
-    image: '',
+    image: '/images/care.jpg',
     tags: [],
     site: 'https://www.care.org/',
     id: 0,
@@ -12,7 +12,7 @@ export const organizations = [
   {
     title: 'International Rescue Committee',
     description: "The International Rescue Committee responds to the world’s worst humanitarian crises and helps people whose lives and livelihoods are shattered by conflict and disaster to survive, recover and gain control of their future.",
-    image: '',
+    image: '/images/int-rescue-committee.jpg',
     tags: [],
     site: 'https://www.rescue.org/',
     id: 1,
@@ -20,7 +20,7 @@ export const organizations = [
   {
     title: 'American Red Cross',
     description: "The American Red Cross is a non-profit humanitarian organization that provides emergency assistance, disaster relief, and disaster preparedness education in the United States.",
-    image: '',
+    image: '/images/american-red-cross.jpg',
     tags: [],
     site: 'https://www.redcross.org/',
     id: 2,
@@ -28,7 +28,7 @@ export const organizations = [
   {
     title: 'The World Food Programme',
     description: "The World Food Programme is the food-assistance branch of the United Nations. It is the world's largest humanitarian organization, the largest one focused on hunger and food security, and the largest provider of school meals.",
-    image: '',
+    image: '/images/wfp.jpg',
     tags: [],
     site: 'https://www.wfpusa.org/',
     id: 3,
@@ -36,7 +36,7 @@ export const organizations = [
   {
     title: 'Doctors Without Borders',
     description: "Médecins Sans Frontières, sometimes rendered in English as Doctors Without Borders, is an international humanitarian medical non-governmental organisation of French origin best known for its projects in conflict zones and in countries affected by endemic diseases.",
-    image: '',
+    image: '/images/doctors-without-borders-sq-new.jpg',
     tags: [],
     site: 'https://www.doctorswithoutborders.org/',
     id: 4,
@@ -44,7 +44,7 @@ export const organizations = [
   {
     title: 'Oxfam',
     description: "Oxfam is a British founded confederation of 20 independent charitable organizations focusing on the alleviation of global poverty, founded in 1942 and led by Oxfam International. It is a major nonprofit group with an extensive collection of operations.",
-    image: '',
+    image: '/images/oxfam.jpg',
     tags: [],
     site: 'https://www.oxfamamerica.org/',
     id: 5,
@@ -53,7 +53,7 @@ export const organizations = [
   {
     title: 'The Nature Conservancy',
     description: "Our mission is to conserve the lands and waters on which all life depends. To achieve this, we must boldly address the biodiversity and climate crises over the next decade.",
-    image: '',
+    image: '/images/nature-conservancy.jpg',
     tags: [],
     site: 'https://www.nature.org/en-us/',
     id: 6,
@@ -61,7 +61,7 @@ export const organizations = [
   {
     title: 'https://www.nrdc.org/',
     description: "NRDC works to safeguard the earth—its people, its plants and animals, and the natural systems on which all life depends.",
-    image: '',
+    image: '/images/nrdc.jpg',
     tags: [],
     site: 'https://www.nrdc.org/',
     id: 7,
@@ -69,7 +69,7 @@ export const organizations = [
   {
     title: 'World Wildlife Fund',
     description: "Our mission is to conserve nature and reduce the most pressing threats to the diversity of life on Earth.",
-    image: '',
+    image: '/images/wwf.jpg',
     tags: [],
     site: 'https://www.worldwildlife.org/',
     id: 8,
@@ -77,7 +77,7 @@ export const organizations = [
   {
     title: 'The American Society for the Prevention of Cruelty to Animals',
     description: "The American Society for the Prevention of Cruelty to Animals is a non-profit organization dedicated to preventing cruelty to animals.",
-    image: '',
+    image: '/images/aspca.jpg',
     tags: [],
     site: 'https://www.aspca.org/',
     id: 9,
@@ -85,7 +85,7 @@ export const organizations = [
   {
     title: 'Greenpeace',
     description: "Greenpeace is a non-governmental environmental organisation with offices in over 55 countries and an international coordinating body in Amsterdam, the Netherlands. Greenpeace was founded in 1971 by Irving Stowe and Dorothy Stowe, Canadian and US expat environmental activists.",
-    image: '',
+    image: '/images/greenpeace.jpg',
     tags: [],
     site: 'https://www.greenpeace.org/usa/',
     id: 10,
@@ -93,20 +93,20 @@ export const organizations = [
   {
     title: 'The Sierra Club',
     description: "The Sierra Club is the most enduring and influential grassroots environmental organization in the United States. We amplify the power of our 3.8 million members and supporters to defend everyone’s right to a healthy world.",
-    image: '',
+    image: '/images/sierra-club.jpg',
     tags: [],
     site: 'https://www.sierraclub.org/',
     id: 11,
   },
   
-  {
-    title: 'Tech Blog',
-    description: "Save information to a database. While sign in a user can make new posts which are in turn saved for future edit and deletion if one chooses to. ",
-    image: '/images/tech.png',
-    tags: ['Javacript ', 'Node.js', 'MySQL', 'Sequelize'],
-    source: 'https://only-tech-news.herokuapp.com',
-    id: 7,
-  },
+  // {
+  //   title: 'Tech Blog',
+  //   description: "Save information to a database. While sign in a user can make new posts which are in turn saved for future edit and deletion if one chooses to. ",
+  //   image: '/images/tech.png',
+  //   tags: ['Javacript ', 'Node.js', 'MySQL', 'Sequelize'],
+  //   source: 'https://only-tech-news.herokuapp.com',
+  //   id: 7,
+  // },
 
 ];
 
