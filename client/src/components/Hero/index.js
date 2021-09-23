97b7b979bc8e439883a7abb6490aeaf665a1268f
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-09-23 12:22:02 -0600

    Started the Hero section

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import { HeroContainer, VideoBox, VideoBG, H1, HeroP, HeroBtnWrapper, CalltoActionbtn } from './HeroStyles';
+import Video from '../../videos/video.mp4';
 
 const Hero = () => (
-  <>
+  <HeroContainer id="home">
+    <VideoBox>
+      <VideoBG autoPlay loop muted src={Video} type='video/mp4' />
+    </VideoBox>
+    <HeroContent>
+      <H1>Not for self, but for others</H1>
+      <HeroP>
+        Feeling charitable? Find a cause and make a difference.
+      </HeroP>
+      <HeroBtnWrapper>
+        <CalltoActionbtn to= 'charitablecauses'>
 
-  </>
+        </CalltoActionbtn>
+      </HeroBtnWrapper>
+
+    </HeroContent>
+
+  </HeroContainer>
 );
 
 export default Hero;
