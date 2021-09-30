ebf663e1d67c0a7e35417cc9a8c48ee6cf63aa5d
Aloha <utah.clayton@gmail.com>
2021-09-29 20:41:32 -0700

    apollo import to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,59 @@ import Organizations from "./components/Organizations";
 import Header from "./components/Header";
 import Signup from "./components/Signup";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages";
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-      <div>
-        <Header></Header>
-        <main>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/Charitycauses" component={Charitycauses} />
-            <Route exact path="/Organizations" component={Organizations} />
-            <Route exact path="/Signup" component={Signup} />
-            <Route />
-            <Route />
-          </Switch>
-        </main>
-        <Footer></Footer>
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Header></Header>
+          <main>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/Charitycauses" component={Charitycauses} />
+              <Route exact path="/Organizations" component={Organizations} />
+              <Route exact path="/Signup" component={Signup} />
+              <Route />
+              <Route />
+            </Switch>
+          </main>
+          <Footer></Footer>
+        </div>
+      </Router>
+    </ApolloProvider>
+
   );
 }
 
