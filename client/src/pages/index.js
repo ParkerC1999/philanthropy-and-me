2529cd3a23f8cb292bba10826146216afc8f87dc
ParkerC1999 <parkman.christiansen@gmail.com>
2021-10-03 17:42:30 -0600

    comment stuff out

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,14 +3,14 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
 // import Donation from "../components/Donation";
-import Footer from "../components/Footer";
-import Charitycauses from "../components/Charitycauses"
+// import Footer from "../components/Footer";
+// import Charitycauses from "../components/Charitycauses"
 import {
   homeObjOne,
   homeObjTwo,
   homeObjThree,
 } from "../components/About/template";
-import Organizations from "../components/Organizations";
+// import Organizations from "../components/Organizations";
 
 function Home() {
   const [isOpen, setIsOpen] = useState(false);
